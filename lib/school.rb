517d7098b567be45cf685fd87e277ded59bdabec
courserav <lvsmlit@gmail.com>
2020-12-19 21:52:02 -0500

    commit tree aa230a57487f15e87a99857e08872c3ee4909e8b

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+    def initialize(name)
+        @name = name
+        @hash = {}
+    end
+
+    def roster
+        @hash
+    end
+
+    def add_student(student_name, grade)
+        @hash[grade] ||= []
+        @hash[grade] << student_name
+    end
+
+    def grade(year)
+        @hash[year]
+    end
+
+    def sort
+        @hash.each do |grade, student_array|
+            @hash[grade] = student_array.sort
+        end
+        @hash
+    end
+end
